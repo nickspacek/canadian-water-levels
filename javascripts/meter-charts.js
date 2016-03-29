7a674c49da3df34d5e78750280837a3c594cbcf7
Nick Spacek <nick@spacek.ca>
2016-03-29 13:31:30 +0000

    Updates

---
+++ javascripts/meter-charts.js
@@ -0,0 +1,29 @@
+var meterData = meter.levels.stats.past24Hours.map(function (reading) {
+  return [ new Date(reading.ts), reading.v ];
+});
+
+google.charts.load('current', {'packages':['corechart']});
+    google.charts.setOnLoadCallback(drawChart);
+
+    function drawChart() {
+      var formatter = new google.visualization.NumberFormat({
+        fractionDigits: 4
+      });
+      var data = new google.visualization.DataTable();
+      data.addColumn('datetime', 'Date');
+      data.addColumn('number', 'Level');
+
+      data.addRows(meterData);
+
+      formatter.format(data, 1);
+
+      var options = {
+        title: 'Past 24 Hours',
+        curveType: 'function',
+        vAxis: { format: '#.000' },
+        legend: { position: 'bottom' }
+      };
+
+      var chart = new google.visualization.LineChart(document.getElementById('chart'));
+      chart.draw(data, options);
+    }
